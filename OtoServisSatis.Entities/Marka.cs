cf42d8abe89a8318375ac4a9d025b636c4ed81cd
betul <betul@BETL6CA3>
2023-03-26 17:50:18 +0300

    Add project files.

---
+++ OtoServisSatis.Entities/Marka.cs
@@ -0,0 +1,10 @@
+﻿
+namespace OtoServisSatis.Entities
+{
+    public class Marka:IEntity
+    {
+        //id adı
+        public int Id { get; set; }
+        public string Adi { get; set; }
+    }
+}
---
+++ OtoServisSatis.Entities/Rol.cs
@@ -0,0 +1,9 @@
+﻿
+namespace OtoServisSatis.Entities
+{
+    public class Rol
+    {
+        public int Id { get; set; }
+        public string Adi { get; set; }
+    }
+}
